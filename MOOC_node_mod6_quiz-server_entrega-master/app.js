c7f69ce472c0c092de93aa8c28ae0f885b9d1cfb
SusannaMontero <49920436+SusannaMontero@users.noreply.github.com>
2021-05-03 14:00:02 +0200

    method override MW
    se instala el MW y se importan los paquetes

--- MOOC_node_mod6_quiz-server_entrega-master/app.js
+++ MOOC_node_mod6_quiz-server_entrega-master/app.js
@@ -7,6 +7,10 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+// Añadido
+var methodOverride = require('method-override'); // importamos el paquete instalando el midlleware en nuestra aplicación con app.use
+var routes = require('./routes/index');
+
 var app = express();
 
 // view engine setup
@@ -22,20 +26,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+// Añadido
+app.use(methodOverride('_method'));
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
